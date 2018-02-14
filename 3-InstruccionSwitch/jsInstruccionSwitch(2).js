467c39a773cf78802d04d5a364b80d09296c719a
pablovalenz <36089307+pablovalenz@users.noreply.github.com>
2018-02-14 12:28:32 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,46 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+//alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Julio":
+    alert("Abrigate que hace frio");
+    break;
+    case "Agosto":
+    alert("Abrigate que hace frio");
+    break;
+    case "Enero":
+    alert("Falta para el invierno");
+    break;
+    case "Febrero":
+    alert("Falta para el invierno");
+    break;
+    case "Marzo":
+    alert("Falta para el invierno");
+    break;
+    case "Abril":
+    alert("Falta para el invierno");
+    break;
+    case "Mayo":
+    alert("Falta para el invierno");
+    break;
+    case "Junio":
+    alert("Falta para el invierno");
+    break;
+    case "Septiembre":
+    alert("Ya pasamos el frio, ahora el calor!!!");
+    break;
+    case "Octubre":
+    alert("Ya pasamos el frio, ahora el calor!!!");
+    break;
+    case "Noviembre":
+    alert("Ya pasamos el frio, ahora el calor!!!");
+    break;
+    case "Diciembre":
+    alert("Ya pasamos el frio, ahora el calor!!!");
+    break;
+}
 
 
 
